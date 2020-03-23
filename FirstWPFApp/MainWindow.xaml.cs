886d6bec2bd908fe88465c122b642db0176d3791
Johnathon Crawford <47326832+Kodi125@users.noreply.github.com>
2020-03-23 21:37:18 +0000

    Add project files.

---
+++ FirstWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TbxName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            tbxName.Clear();
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            // read name entered in the text box 
+            string input = tbxName.Text;
+
+            //display message
+           // MessageBox.Show("Hi " + input);
+            MessageBox.Show("Hi " + input, "Information",
+                MessageBoxButton.OKCancel,
+                MessageBoxImage.Information);
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SecondWDFApp/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SecondWDFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRoll_Click(object sender, RoutedEventArgs e)
+        {
+            RollingDice();
+        }
+        public async void RollingDice()
+        {
+
+            Random random = new Random();
+
+            //roll the dice
+            for (int i = 0; i < 20; i++)
+            {
+                int number = random.Next(1, 7);
+                tblkDice.Text = number.ToString();
+                await Task.Delay(50);
+            }//end of loop
+           
+        }
+    }
+}
